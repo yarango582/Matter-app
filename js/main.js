3cb051b6f65502c13e2fbc6ef6d1f05a946ab9f1
Mauricio Guerra <maurogebe.96@gmail.com>
2020-10-19 21:57:08 -0500

    ajustes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-import verificarLogin from "./authenticated.js";
 import LoginUser from "./login.js";
 import RegisterUser from "./register.js";
 import Authenticated from "./authenticated.js";
 import Notificades from "./notificade.js";
 import Feedback from "./Feedback.js";
+
 const login = new LoginUser();
 login.authenticated();
 
@@ -27,29 +27,9 @@ if (window.location.pathname === "/views/register.html") {
 	});
 }
 //notificade
-const notificade = new Notificades();
-notificade.NumberNotificade(7);
-let x = [
-	{
-		id: 1,
-		name: "Comunicaci\u00f3n",
-		created_at: "2020-10-16T04:48:56.000000Z",
-		updated_at: "2020-10-16T04:48:56.000000Z",
-	},
-	{
-		id: 2,
-		name: "Empat\u00eda",
-		created_at: "2020-10-16T04:48:56.000000Z",
-		updated_at: "2020-10-16T04:48:56.000000Z",
-	},
-	{
-		id: 3,
-		name: "Liderazgo",
-		created_at: "2020-10-16T04:48:56.000000Z",
-		updated_at: "2020-10-16T04:48:56.000000Z",
-	},
-];
-notificade.GetNotificade();
+
+
+// notificade.GetNotificade();
 document.getElementById("start").addEventListener("click", () => {
 	const feedback = new Feedback();
 	feedback.getFeedback();
@@ -57,6 +37,8 @@ document.getElementById("start").addEventListener("click", () => {
 document.getElementById("decline").addEventListener("click", () => {
 	storage.removeItem(keyName);
 });
+
+
 
 // const register = new RegisterUser
 
