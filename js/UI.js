d47760dc13d50016845ba83486c088850b43b51a
Yarango <yeisonarango03@gmail.com>
2020-10-21 22:16:53 -0500

    listo

--- js/UI.js
+++ js/UI.js
@@ -28,15 +28,17 @@ export default class UI {
     mostrarMenuCambiarContraseña(){
 
         let menu = document.getElementById('row-form-password');
+        let menuFeed = document.getElementById('row-form-invite-feedback');
 
         menu.style.display = 'none';
-
+        
         if(menu != null){
             document.getElementById('change-password').addEventListener('click', (even) =>{
 
                 switch (menu.style.display) {
                     case 'none':
                         menu.style.display = 'block';
+                        menuFeed.style.display = 'none';
                         break;
                     case 'block':
                         menu.style.display = 'none';
