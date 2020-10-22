b1361a82dec0c000085f4189558309c2c1ff7ea6
Yarango <yeisonarango03@gmail.com>
2020-10-21 19:27:50 -0500

    listo

--- js/UI.js
+++ js/UI.js
@@ -28,10 +28,8 @@ export default class UI {
     mostrarMenuCambiarContraseña(){
 
         let menu = document.getElementById('row-form-password');
-        let menuInvite = document.getElementById('row-form-invite-feedback');
 
         menu.style.display = 'none';
-        menuInvite.style.display = 'none';
 
         if(menu != null){
             document.getElementById('change-password').addEventListener('click', (even) =>{
@@ -39,7 +37,6 @@ export default class UI {
                 switch (menu.style.display) {
                     case 'none':
                         menu.style.display = 'block';
-                        menuInvite.style.display = 'none'
                         break;
                     case 'block':
                         menu.style.display = 'none';
@@ -53,32 +50,5 @@ export default class UI {
 
     }
 
-    mostrarMenuInvitaFeedBack(){
-
-        let menu = document.getElementById('row-form-invite-feedback');
-        let menuPass = document.getElementById('row-form-password');
-
-        menu.style.display = 'none';
-        menuPass.style.display = 'none';
-
-        if(menu != null){
-            document.getElementById('invite').addEventListener('click', (even) =>{
-
-                switch (menu.style.display) {
-                    case 'none':
-                        menu.style.display = 'block';
-                        menuPass.style.display = 'none';
-                        break;
-                    case 'block':
-                        menu.style.display = 'none';
-                        break;
-                    default:
-                        console.log('Error revisa el codigo');
-                        break;
-                }
-            })     
-        }
-
-    }
 
 }
